bfe800f43b73a36f16071fa8a6c06b7bf54557e9
maxencemachu <maxence.machu@akarah.com>
2021-06-16 16:23:10 +0200

    feat(camera)

--- App.js
+++ App.js
@@ -1,14 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button, StyleSheet, Text, View, SafeAreaView,TouchableOpacity, Image, Pressable, ScrollView } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+
 import Home from './pages/Home';
 import DemoState from './pages/DemoState';
 import LoremPicsum from './pages/LoremPicsum';
 import Login from './pages/Login';
+import AppCamera from './pages/AppCamera';
 import Header from './components/Header';
+
 export default function App() {
 
   const [page, navigate] = useState('Home');
+  const [hasPermission, setHasPermission] = useState(null);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
 
   /* Ajout de variables avant le return */
   const items = [
@@ -37,6 +51,32 @@ export default function App() {
 
   function onScanPress(){
     /* TOOD: Ouvrir une page pour l'appareil photo */
+    navigate('Camera');
+  }
+
+  async function onBarCodeScan({data, type}){
+    await getProductInfoFromApi(data);
+    // ICI le code a attendu d'avoir la réponse de l'API
+  }
+
+
+  async function getProductInfoFromApi (barCode) {
+    try {
+      //this.setState({loading : true});
+
+      let response = await fetch(
+        'https://fr.openfoodfacts.org/api/v0/produit/' + barCode + '.json'
+      );
+      let responseJson = await response.json();
+
+      // Travailler avec les données 
+      let _products = products;
+      _products.push(responseJson);
+      setProducts(_products);
+
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   /* Fonction Appel API */
@@ -45,12 +85,13 @@ export default function App() {
     <SafeAreaView style={styles.container}>
         <StatusBar style="auto" />
         {/* Header */}
-        <Header title="Homepage"/>
+        <Header title={page}/>
 
         {/* TODO: Ajouter navigation */}
         {page === 'Home' && <Home appScan={onScanPress} appItems={items}/>}
-        {page === 'DemoState' &&  <DemoState/>}
-        {page === 'LoremPicsum' &&  <LoremPicsum/>}
+        {page === 'Camera' && <AppCamera handleBarCodeScanned={onBarCodeScan}/>}
+{/*         {page === 'DemoState' &&  <DemoState/>}
+        {page === 'LoremPicsum' &&  <LoremPicsum/>} */}
         {page === 'Login' &&  <Login login={() => {alert('Je suis connecté !'); navigate('Home')}}/>}
       
 
@@ -60,7 +101,7 @@ export default function App() {
             color={page === "Home" ? "green" : 'grey'}
             onPress={() => navigate('Home')}
             />
-          <Button 
+          {/* <Button 
             title="State" 
             color={page === "DemoState" ? "green" : 'grey'}
             onPress={() => navigate('DemoState')}
@@ -70,7 +111,7 @@ export default function App() {
             title="Picsum" 
             color={page === "LoremPicsum" ? 'green' : 'grey'}
             onPress={() => navigate('LoremPicsum')}
-            />
+            /> */}
 
           <Button 
             title="Login" 
